504562b5fe5b59d276a66ab1ed041c18f446b426
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2024-05-04 17:56:03 +0900

    サービスプロバイダーテスト

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
+    public function showServiceProviderTest()
+    {
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+        dd($sample,$password,$encrypt->decrypt($password));
+    }
+
     public function showComponent1()
     {
         return view('tests.test1');
