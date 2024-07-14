84c9fc25cc63c8f9dd77da1947dda42a51db32c0
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2024-07-14 18:38:14 +0900

    intevension

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,8 @@ use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class ShopController extends Controller
 {
@@ -36,9 +38,19 @@ class ShopController extends Controller
 
     public function update(Request $request,$id) {
         $imageFile = $request->image;
-        if(!is_null($imageFile) && $imageFile->isValid()) {
-            Storage::putFile('public/shops',$imageFile);
-        }
+        // if(!is_null($imageFile) && $imageFile->isValid()) {
+        //     Storage::putFile('public/shops',$imageFile);
+        // }
+
+        $fileName = uniqid(rand().'_');
+        $extension = $imageFile->extension();
+        $fileNameStore = $fileName.'.'.$extension;
+
+        $manager = new ImageManager(new Driver());
+        $img = $manager->read($imageFile);
+        $resizeImage = $img->resize(1920,1080)->encode();
+
+        Storage::put('public/shops/'.$fileNameStore,$resizeImage);
 
         return to_route('owner.shops.index');
     }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\SampleServiceProvider::class,
+    // Intervention\image\ImageServiceProvider::class
+    
 ];
