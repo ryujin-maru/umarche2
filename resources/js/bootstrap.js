c554af9fddd083dbfea08028bb1cf3442df669b3
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2024-09-16 20:27:37 +0900

    micromodal

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+import MicroModal from 'micromodal';
+MicroModal.init({
+    disableScroll:true
+});
--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -30,6 +30,8 @@
                                     </select>
                                 </div>
                             </div>
+
+                            <x-select-image name="image1" :images="$images"/>
                             <div class="p-2 w-full mt-4 flex justify-around">
                                 <button type="button" onclick="location.href='{{route('owner.products.index')}}'" class="text-gray-200 bg-gray-400 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
@@ -40,4 +42,21 @@
             </div>
         </div>
     </div>
+<script>
+    'use strict'
+    const images = document.querySelectorAll('.image');
+    images.forEach(image => {
+        image.addEventListener('click',function(e) {
+            const imageName = e.target.dataset.id.substr(0,6)
+            const imageId = e.target.dataset.id.replace(imageName+'_','')
+            const imageFile = e.target.dataset.file
+            const imagePath = e.target.dataset.path
+            const modal = e.target.dataset.modal
+
+            document.getElementById(imageName+'_thumbnail').src = imagePath+'/'+imageFile
+            document.getElementById(imageName+'_hidden').value = imageId
+            MicroModal.close(modal);
+        })
+    })
+</script>
 </x-app-layout>
