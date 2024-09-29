e9b4caba3f6dec686017387ed5864ba338a69dcf
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2024-09-29 17:06:50 +0900

    product edit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -30,7 +30,16 @@ class Product extends Model
         return $this->belongsTo(SecondaryCategory::class,'secondary_category_id');
     }
     public function imageFirst() {
+        return $this->belongsTo(Image::class,'image2','id');
+    }
+    public function imageSecond() {
         return $this->belongsTo(Image::class,'image1','id');
+    }
+    public function imageThird() {
+        return $this->belongsTo(Image::class,'image3','id');
+    }
+    public function imageForth() {
+        return $this->belongsTo(Image::class,'image4','id');
     }
 
     public function stock() {
