69d9678ca7e09dbe2204dc9130e9f2ceebeaf670
徳永龍仁 <juktwmh@icloud.com>
2024-12-30 18:09:19 +0900

    stripe previous

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Constants\Common;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\Stock;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -52,17 +54,32 @@ class CartController extends Controller
 
         $lineItems = [];
         foreach($products as $product) {
-            $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity
-            ];
+            $quantity = '';
+            $quantity = Stock::where('product_id',$product->id)->sum('quantity');
 
-            array_push($lineItems,$lineItem);
+            if($product->pivot->quantity > $quantity){
+                return to_route('user.cart.index');
+            }else{
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity
+                ];
+    
+                array_push($lineItems,$lineItem);
+            }
         }
-        
+
+        foreach($products as $product) {
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => Common::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1,
+            ]);
+        }
+        dd('test');
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
         $session = \Stripe\Checkout\Session::create([
