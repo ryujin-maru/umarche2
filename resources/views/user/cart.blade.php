586b0a470cda428402ae1a61977e8a4a89eb2c4c
徳永龍仁 <juktwmh@icloud.com>
2024-12-31 15:39:37 +0900

    決済処理後の削除

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -37,8 +37,8 @@
                     <div class="my-2">
                         小計:{{number_format($totalPrice)}}<span class="text-sm text-gray-700">円(税込)</span>
                     </div>
-                    <div>
-                        <button class="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded" onclick="location.href='{{route('user.cart.checkout')}}'">購入する</button>
+                    <div class="text-end">
+                        <button class=" text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded" onclick="location.href='{{route('user.cart.checkout')}}'">購入する</button>
                     </div>
                     @else
                     カートに商品が入っていません
