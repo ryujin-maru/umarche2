82f5c5383eca4226b0494ada97eb646f93460e21
徳永龍仁 <juktwmh@icloud.com>
2024-12-31 15:47:18 +0900

    キャンセル処理

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::prefix('cart')->middleware('auth:users')->group(function() {
 
     Route::get('checkout',[CartController::class,'checkout'])->name('cart.checkout');
     Route::get('success',[CartController::class,'success'])->name('cart.success');
+    Route::get('cancel',[CartController::class,'cancel'])->name('cart.cancel');
 });
 
 // Route::get('/dashboard', function () {
